38788061e222019ad8801b01c342b3efde5b2046
singh431himanshu-commits <singh431himanshu@gmail.com>
2026-07-31 12:13:47 +0530

    Updated files

--- finance_bot.py
+++ finance_bot.py
@@ -100,6 +100,7 @@ def generate_finance_draft(topic_context):
     """
 
     try:
+        client = get_next_client() # हर बार नई API Key ले लेगा``
         response = client.chat.completions.create(
             model="llama-3.3-70b-versatile",
             messages=[{"role": "user", "content": prompt}],
